c6d14a742c0be8c7b1f02314ef8f9e8413450cab
loginlife <hxstrive@outlook.com>
2017-07-30 19:19:17 +0800

    在Test2.java文件中添加show()方法

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,14 @@
+import java.lang.StringBuffer;
+
 public class Test2 {
 
     public static void main(String[] args) {
         //
+    }
+
+    public void show(String msg) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("message:").append(msg);
+        System.out.println(buffer.toString());
     }
 }
